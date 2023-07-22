c458af5964a2b27a2548e7ab12f412f8ebe53180
YousefMaher179 <ym1233445@gmail.com>
2023-07-22 22:57:14 +0300

    fix

--- client/src/routes/NewPost.jsx
+++ client/src/routes/NewPost.jsx
@@ -1,12 +1,12 @@
-import { Link, Form, redirect } from 'react-router-dom';
+import { Link, Form, redirect } from "react-router-dom";
 
-import classes from './NewPost.module.css';
-import Modal from '../components/Modal';
+import classes from "./NewPost.module.css";
+import Modal from "../components/Modal";
 
 function NewPost() {
   return (
     <Modal>
-      <Form method='post' className={classes.form}>
+      <Form method="post" className={classes.form}>
         <p>
           <label htmlFor="body">Text</label>
           <textarea id="body" name="body" required rows={3} />
@@ -28,16 +28,16 @@ function NewPost() {
 
 export default NewPost;
 
-export async function action({request}) {
+export async function action({ request }) {
   const formData = await request.formData();
   const postData = Object.fromEntries(formData); // { body: '...', author: '...' }
-  await fetch('http://localhost:8080/posts', {
-    method: 'POST',
+  await fetch("/posts", {
+    method: "POST",
     body: JSON.stringify(postData),
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   });
 
-  return redirect('/');
+  return redirect("/");
 }
