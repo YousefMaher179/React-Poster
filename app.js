c09939119fcfe635ffeebd91645796d79a8e9cac
YousefMaher179 <ym1233445@gmail.com>
2023-07-22 22:35:34 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+const { getStoredPosts, storePosts } = require("./data/posts");
+
+const app = express();
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.get("/posts", async (req, res) => {
+  const storedPosts = await getStoredPosts();
+  res.json({ posts: storedPosts });
+});
+
+app.get("/posts/:id", async (req, res) => {
+  const storedPosts = await getStoredPosts();
+  const post = storedPosts.find((post) => post.id === req.params.id);
+  res.json({ post });
+});
+
+app.post("/posts", async (req, res) => {
+  const existingPosts = await getStoredPosts();
+  const postData = req.body;
+  const newPost = {
+    ...postData,
+    id: Math.random().toString(),
+  };
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+  });
+
+  const updatedPosts = [newPost, ...existingPosts];
+  await storePosts(updatedPosts);
+  res.status(201).json({ message: "Stored new post.", post: newPost });
+});
+
+const port = 8080;
+
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`server is listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
